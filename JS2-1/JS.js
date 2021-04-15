7347f8e952154c7cc79599d8d18d4176b7b9808e
2theMoon <dlaguddh1@gmail.com>
2021-04-15 15:21:38 +0900

    d

---
+++ JS2-1/JS.js
@@ -0,0 +1,28 @@
+
+const calculator = {
+    plus: function(a , b){
+        return a + b;
+    },
+    minus: function(a , b){
+        return a - b;
+    },
+    mul: function(a , b){
+        return a * b;
+    },
+    div: function(a , b){
+        return a / b;
+    }
+
+    
+    
+}
+
+//console.log(greetheMoon);
+const Cplus = calculator.plus(5, 5);
+console.log(Cplus);
+const Cminus = calculator.minus(5, 5);
+console.log(Cminus);
+const Cmul = calculator.mul(5, 5);
+console.log(Cmul);
+const Cdiv = calculator.div(5, 5);
+console.log(Cdiv);
