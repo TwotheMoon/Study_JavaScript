cdcca040deafc09034081fafc79dde3faac76b6b
2theMoon <dlaguddh1@gmail.com>
2021-06-11 17:42:22 +0900

    update

---
+++ JS1-1/app.js
@@ -0,0 +1,17 @@
+const a = 5;    // const = 상수
+const b = 2;
+
+let myName = "moon";    // let = 변수
+
+var c = 10;     // var = 재할당 가능한 변수
+
+// 사용 빈도 1.const 2.let 3.var(되도록 사용X)
+
+console.log(a + b);     //console.log 콘솔에 로그 띄우기
+console.log(a * b);
+console.log(a / b);
+console.log("hellow " + myName);
+
+myName = "the moon";
+console.log("my name is " + myName);
+
